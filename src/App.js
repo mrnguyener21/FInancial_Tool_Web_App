a68607f1b5cdd0bb60ab989ad08813a7dd5f5c08
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-05-20 19:56:53 -0700

    placed each component inside a modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,120 @@
 import React from 'react';
 import {BudgetCalculator, CompoundInterestCalculator, RentCalculator} from './Components';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+
+
+
 const App = ()=> {
+
+  //probably take useStyles and transition it to app.module.css later
+  const useStyles = makeStyles((theme) => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+  const classes = useStyles();
+  const [budget, setBudget] = React.useState(false);
+  const [rent, setRent] = React.useState(false);
+  const [compoundInterest, setCompoundInterest] = React.useState(false);
+
+  const openBudget = () => {
+    setBudget(true)
+  
+  };
+  const openRent = () => {
+    setRent(true)
+  
+  };
+  const openCompoundInterest = () => {
+    setCompoundInterest(true)  
+  
+  };
+
+  const handleClose = () => {
+    setBudget(false);
+    setRent(false);
+    setCompoundInterest(false);
+  };
   return (
     <div className="App">
      <h1>APP</h1>
+
+     <button type="button" onClick={openBudget}>
+        Budget Calculator
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={budget}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={budget}>
+          <div className={classes.paper}>
      <BudgetCalculator/>
-     <CompoundInterestCalculator/>
+          </div>
+        </Fade>
+      </Modal>
+     <button type="button" onClick={openRent}>
+        Rent Calculator
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={rent}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={rent}>
+          <div className={classes.paper}>
      <RentCalculator/>
+          </div>
+        </Fade>
+      </Modal>
+     <button type="button" onClick={openCompoundInterest}>
+        Compound Interest Calculator
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={compoundInterest}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={compoundInterest}>
+          <div className={classes.paper}>
+     <CompoundInterestCalculator/>
+          </div>
+        </Fade>
+      </Modal>
     </div>
   );
 }
