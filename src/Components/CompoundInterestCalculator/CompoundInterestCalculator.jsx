62b7035c00b112b307350392b46a46de8ac0a954
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-05-20 21:17:04 -0700

    added compound interest modal

--- src/Components/CompoundInterestCalculator/CompoundInterestCalculator.jsx
+++ src/Components/CompoundInterestCalculator/CompoundInterestCalculator.jsx
@@ -1,11 +1,94 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CompoundInterestChart from './CompoundInterestChart/CompoundInterestChart';
+
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const CompoundInterestCalculator = () => {
-    return(
+  const [initialInvestment, setInitialInvestment]= useState('');
+  const [yearsInvested,setYearsInvested] =useState('');
+  const [interestRate, setInterestRate] =useState('');
+  const [compoundValue, setCompoundValue] = useState({rate:''})
+
+  
+  //conversion 
+  const principal = parseInt(initialInvestment);
+  const interest = (interestRate/100);
+  const years = parseInt(yearsInvested)
+  const compoundRate = parseInt(compoundValue.rate);
+
+
+  //NEED TO MAKE THESE DYNAMIC WITH FOR LOOP TO GET DATA FOR EACH YEAR. FOR EACH ITERATION WE WILL WANT TO PUSH IT INTO AN EMPTY ARRAY, JK WE DON'T NEED TO PUSH TO AN EMPTY ARRAY IF WE JUST USE MAP FOR EACH ITERATION...LETS THINK THIS THROUGH
+  //WE WILL ALSO WANT TO PUSH EACH ITERATION INTO AN EMPTY ARRAY SO THAT OUR LABELS CAN BE DYNAMIC AS WELL
+  //formula for simple interest
+  const totalWithInterest = [principal];
+
+  for (let i = 1; i < years+1; i++) {
+    totalWithInterest.push(principal+(principal*interest)*i)
+  }
+
+  //formula for compound interest
+  //pretty sure if i put all variables in a for loop and replace the years with i, it should be good
+  const totalWithCompoundInterst = [principal];
+  const arrayOfYears =[0]
+  for (let i = 1; i < years+1; i++) {
+    const a = interest / compoundRate;
+    const b = 1 + a;
+    const c = compoundRate * i;
+    const d = Math.pow(b, c);
+     totalWithCompoundInterst.push((principal * d).toFixed(2));
+     arrayOfYears.push(i);
+    
+  }
+
+  //to correctly pass the values based on which compound rate is chosen
+  const handleChange = (event) => {
+    const name = event.target.name;
+    setCompoundValue({
+      ...compoundValue,
+      [name]: event.target.value,
+    });
+  };  
+
+  return(
+    <div>
+      <div>
         <div>
-            <h1>CompoundInterestCalculator</h1>
+          <p>STEP 1 INITIAL INVESTMENT</p>
+          <input type={'text'} placeholder={'Inital Investment*'} value={initialInvestment}  onChange={(e)=>setInitialInvestment(e.target.value)} />
+        </div>
+        <div>
+            <p>STEP 2 CONTRIBUTE</p>
+          <input type={'text'} placeholder={'years Invested*'} value={yearsInvested}  onChange ={(e)=>setYearsInvested(e.target.value)} />
+        </div>
+        <div>
+          <p>STEP 3 INTEREST RATE</p>
+          <input type={'text'} placeholder={'Interest Rate*'} value={interestRate} onChange={(e)=>setInterestRate(e.target.value)} />
+        </div>
+        <div>
+          <p>STEP 4 COMPOUND IT</p>
+            <FormControl>
+              <InputLabel>Compound Rate</InputLabel>
+              <Select
+                native
+                value={compoundValue.rate}
+                onChange={handleChange}
+                name="rate"
+              >
+                <option aria-label="None" value="" />
+                <option value={1}>Annual</option>
+                <option value={2}>Semi-Annual</option>
+                <option value={4}>Quarterly</option>
+                <option value={12}>Monthly</option>
+              </Select>
+            </FormControl>
+          </div>    
+      </div>
+            <CompoundInterestChart totalWithInterest={totalWithInterest} totalWithCompoundInterst={totalWithCompoundInterst} principal={principal} years={years} interest={interest} compoundRate={compoundRate} arrayOfYears={arrayOfYears} />
         </div>
     )
-}
+  }
+
 
 export default CompoundInterestCalculator;
---
+++ src/Components/CompoundInterestCalculator/CompoundInterestChart/CompoundInterestChart.jsx
@@ -0,0 +1,67 @@
+import React,{useState} from 'react';
+import {Line} from 'react-chartjs-2';
+
+
+
+const CompoundInterestChart = ({totalWithInterest, totalWithCompoundInterst, principal, years, interest, compoundRate, arrayOfYears}) => {
+  const [click, setClick] =useState(false);
+
+  const data = {
+      labels: arrayOfYears,// can work dynamically
+      datasets: [
+        {
+          label: 'Total Amount With Interest',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',//refers to the color of the point
+          borderColor: 'rgba(75,192,192,1)',// refers to line border
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(0,0,0)',
+          pointBorderWidth: 2,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: totalWithInterest
+        },
+        {
+          label: 'Total Amount With Comound Interest',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor:'#FFCE56',//refers to the color of the point
+          borderColor: '#FFCE56',// refers to line border
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(0,0,0)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 2,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: totalWithCompoundInterst
+        },
+      ]
+    };
+  
+    return(
+      <>
+      <button onClick={()=>{principal > 0 && years > 0 && interest > 0 && compoundRate > 0 ?(setClick(true) ) : console.log('fail')}}>CALCULATE</button>
+
+      {click?(
+          <Line data={data}/>
+      ):console.log('no click')}
+      </>
+    )
+}
+
+export default CompoundInterestChart;
