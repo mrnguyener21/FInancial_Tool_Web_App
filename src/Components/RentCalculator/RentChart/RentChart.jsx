5bb2b74940da29b1d7e2da1316ed57138ef35752
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-06-04 15:11:39 -0700

    fixed responsive issue with rental modal graph

--- src/Components/RentCalculator/RentChart/RentChart.jsx
+++ src/Components/RentCalculator/RentChart/RentChart.jsx
@@ -67,7 +67,7 @@ const muiTheme = createMuiTheme({
           />
         </ThemeProvider>
         <Typography id="discrete-slider-small-steps" gutterBottom>
-          *Percentage of Monthly Income Going Towards* 
+          *Percentage of Income Taxed* 
         </Typography>
       </div>
       <div className={styles.buttonContainer}>
